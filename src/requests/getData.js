46a50d6bb4e928a29abd67edc6413ebfce47f2f5
RobertoDeAbreuSalgado <r.salgado3@gmail.com>
2020-10-06 10:55:23 -0300

    Adicionando hooks, url raiz da api e requisição de dados.

---
+++ src/requests/getData.js
@@ -0,0 +1,17 @@
+import axios from "axios"
+import { BASE_URL } from "../constants/urls"
+
+const getData = (endpoint, setData) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    axios.get(`${BASE_URL}${endpoint}`, {
+      headers: {
+        Authorization: token
+      }
+    })
+      .then(response => setData(response.data))
+      .catch(err => console.log(err.message))
+  }
+}
+
+export default getData
