5a6a3d8d4275c1bfadf6af910e4a00c99d86e6bb
GuilhermeGuyLouis <guigl8@hotmail.com>
2020-10-06 16:29:20 -0300

    splah page pronta

--- src/pageScreens/splashPage/SplashPage.jsx
+++ src/pageScreens/splashPage/SplashPage.jsx
@@ -1,10 +1,24 @@
 import React from "react"
+import Logo from "./Img/logo.png"
+import { LogoImage, Container } from "./styled"
+import { goToLoginPage } from "../../routes/Cordinator"
+import { useHistory } from "react-router-dom";
+import InputBase from '@material-ui/core/InputBase';
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
 const SplashPage = () => {
+    
+    const history = useHistory();
+
+    setTimeout(function(){
+        goToLoginPage(history); 
+   }, 3000);
+
+
     return (
-        <div>
-            SplashPage
-        </div>
+        <Container>
+            <LogoImage src={Logo} />          
+        </Container>
     )
 }
 
